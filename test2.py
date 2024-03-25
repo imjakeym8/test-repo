1f2a9de64f7095981a53c236ba94fefb7e2d2352
Jakey <111139747+imjakeym8@users.noreply.github.com>
2024-03-26 02:54:11 +0800

    Added comments to test2.py

--- test2.py
+++ test2.py
@@ -9,19 +9,22 @@ import os
 #)
 my_secret = os.environ['PRIVATE_KEY']
 
+# Chat group I'm basing on @anothertrashgroup
 chat_id = -1001936065448
 
+# Mutes users on TG
 no_permission = ChatPermissions(can_send_messages=False)
+
+# Unmutes users on TG
 default_permissions = ChatPermissions(can_send_messages=True)
 
-
+#Inside bot DM function only
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  
     markup = [
-      [InlineKeyboardButton("1️⃣", callback_data="1")],
-      [InlineKeyboardButton("2️⃣", callback_data="2")],
-      [InlineKeyboardButton("3️⃣", callback_data="3")],
-      [InlineKeyboardButton("4️⃣", callback_data="4")]
+        [InlineKeyboardButton("1️⃣", callback_data="1")],
+        [InlineKeyboardButton("2️⃣", callback_data="2")],
+        [InlineKeyboardButton("3️⃣", callback_data="3")],
+        [InlineKeyboardButton("4️⃣", callback_data="4")]
       ]
     reply_markup = InlineKeyboardMarkup(markup)
 
@@ -29,6 +32,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     member_status = await context.bot.get_chat_member(chat_id, user_id)
 
     #print(member_status.status)
+    # Checks if user is muted and already in the group. /start does not work if user left, or unmuted already.
     if member_status.status == 'restricted' and not member_status.can_send_messages:
       #print(True)
       with open("bananacat.jpg", "rb") as photo:
@@ -40,13 +44,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     #    print(False)
     #print("/start - command working")
 
-
+#Event-based function
 async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    markup = [[
-        InlineKeyboardButton("Confirm your identification here",
-                             url="https://t.me/tgspamcleanserbot?start=start")
-    ]]
+    markup = [
+      [InlineKeyboardButton("Confirm your identification here", url="https://t.me/tgspamcleanserbot?start=start")]
+    ]
     reply_markup = InlineKeyboardMarkup(markup)
 
     joined = update.message.new_chat_members
@@ -55,14 +57,13 @@ async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     for member in joined:
       if member.id != context.bot.id:
-        await context.bot.restrict_chat_member(chat_id, member.id, no_permission)
+        await context.bot.restrict_chat_member(chat_id, member.id, no_permission) # Mutes user after joining
 
     each_username = ', '.join([f"@{username}" for username in new_members])
-    await update.message.reply_text(f"Welcome {each_username}. Go here ⏬",
-                                    reply_markup=reply_markup)
+    await update.message.reply_text(f"Welcome {each_username}. Go here ⏬", reply_markup=reply_markup)
     #print("/welcome - command triggered")
 
-
+#Callback Query for Buttons function
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     query.answer()
@@ -75,7 +76,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
       await query.message.reply_text("Wrong 3️⃣")
     elif query.data == "4":
       user_id = query.from_user.id
-      await context.bot.restrict_chat_member(chat_id, user_id, default_permissions)
+      await context.bot.restrict_chat_member(chat_id, user_id, default_permissions) # Unmutes user
       await query.message.reply_text("Correct 4️⃣. You can go back in the chats!")
 
 def main():
