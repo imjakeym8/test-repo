c7ef8beaeb857388d9d155f5d50e62eda9d84332
Jakey <jc.tacogue@gmail.com>
2024-10-22 18:02:45 +0800

    scratch script for testing logic

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-class Some:
-    def __init__(self) -> None:
-        self.value = {"key":"value"}
+list_of_dicts = [
+{"type":"character","counter":0,"trigger":True},
+{"type":"character","counter":0,"trigger":True},
+{"type":"character","counter":0,"trigger":True},
+{"type":"character","counter":0,"trigger":True}
+]
+another_dicts = [
+{"type":"event","counter":1000,"trigger":True},
+{"type":"event","counter":1000,"trigger":True},
+{"type":"event","counter":1000,"trigger":True},
+]
 
-some = Some()
-a_list = []
 
-a_list.extend([some.value] * 1)
-print(a_list)
+new_list = [ each["counter"] for each in list_of_dicts + another_dicts if each["counter"] < 1000 and each["type"] == "event"]
+new_ratio = len(new_list) / 10 * 100 if new_list != [] else 0
+print(new_ratio)
